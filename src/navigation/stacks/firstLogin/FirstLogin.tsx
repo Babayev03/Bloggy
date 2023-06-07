a4471a25642fb0501e0cf3aedc1386d36e3f3c6e
Babayev03 <ilhambabayev222@gmail.com>
2023-06-07 10:28:40 -0700

    updated code

--- src/navigation/stacks/firstLogin/FirstLogin.tsx
+++ src/navigation/stacks/firstLogin/FirstLogin.tsx
@@ -1,20 +1,38 @@
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
+import SvgGoogle from '../../../assets/images/GoogleIcon';
+import {ThemedButton} from 'react-native-really-awesome-button';
 
 const FirstLogin = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={{fontSize:25,fontFamily:'Raleway-Bold'}}>Welcome!</Text>
+      <View style={styles.imageView}>
+        <Text style={styles.welText}>Welcome!</Text>
       </View>
-      <View style={{width:150}}>
-        <Text style={{fontSize:30,fontFamily:'Raleway-Bold'}}>Log in to get started</Text>
+      <View style={styles.logView}>
+        <Text style={styles.logText}>Log in to get started</Text>
       </View>
       <View>
         <Image
           source={require('../../../assets/onboard/images/loginImage.png')}
-          style={{width: 350, height: 350}}
+          style={styles.image}
         />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <SvgGoogle />
+        <Text style={styles.ButtonText}>Connect With Google</Text>
+      </TouchableOpacity>
+      <View style={styles.slicer}>
+        <Text style={styles.slicerText}>or</Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.ButtonText}>Log In With Your Email</Text>
+      </TouchableOpacity>
+      <View style={styles.singUp}>
+        <Text style={styles.signUpText}>Dont have an account?</Text>
+        <TouchableOpacity style={styles.signNext}>
+          <Text style={{color:"green"}}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -30,4 +48,75 @@ const styles = StyleSheet.create({
   text: {
     color: '#000',
   },
+  logText: {
+    fontSize: 35,
+    fontFamily: 'Raleway-Bold',
+    color: '#e3e3e3',
+  },
+  logView: {
+    marginHorizontal: 25,
+    width: 300,
+  },
+  welText: {
+    fontSize: 25,
+    fontFamily: 'Raleway-Light',
+    color: '#fff',
+    fontWeight: '600',
+    opacity: 0.6,
+  },
+  imageView: {
+    marginTop: 55,
+    marginHorizontal: 25,
+  },
+  image: {
+    width: 350,
+    height: 350,
+  },
+  button: {
+    backgroundColor: '#fff',
+    marginHorizontal: 25,
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 8,
+    gap: 10,
+  },
+  ButtonText: {
+    fontSize: 20,
+    fontFamily: 'Raleway-Medium',
+    color: '#000',
+    paddingVertical: 10,
+  },
+  slicer: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  slicerText: {
+    fontSize: 20,
+    fontFamily: 'Raleway-Medium',
+    color: '#fff',
+    opacity: 0.6,
+  },
+  singUp: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  signUpText: {
+    fontSize: 20,
+    fontFamily: 'Raleway-Medium',
+    color: '#fff',
+    opacity: 0.6,
+  },
+  signNext:{
+    fontSize: 20,
+    fontFamily: 'Raleway-Medium',
+    color: '#fff',
+    marginLeft: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:5
+  }
 });
